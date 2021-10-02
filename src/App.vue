31e9a94376760f86211dee3add6eaf4eb60e3050
Natan Menezes <natanmenezes31@gmail.com>
2021-10-02 15:48:43 -0300

    Criado o Footer

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,19 @@
     <v-main>
       <router-view></router-view>
     </v-main>
+    <Footer/>
   </v-app>
 </template>
 
 <script>
 import AppTopBar from './components/shared/AppTopBar.vue'
+import Footer from './components/shared/Footer.vue'
 
 export default {
   name: 'App',
   components:{
-    AppTopBar    
+    AppTopBar,
+    Footer   
   }
 };
 </script>
---
+++ src/components/shared/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-footer padless style="background-color: #fecf0a">
+    <v-col class="text-center" cols="12">
+      <a href="https://github.com/JordanElizeu/ProjetoHackathon-Iteris" style="text-decoration:none; color:black"><p style="text-align: center">
+        Link do projeto:
+        <v-icon size="24px"> mdi-github </v-icon>
+      </p></a>
+      {{ new Date().getFullYear() }} — <strong>Time 3</strong>
+    </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style scoped>
+</style>
