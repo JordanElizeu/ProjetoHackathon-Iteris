3bc53e5a3135956f01bbaae6772ed4e3d615d8e3
Jordan <jordan.elizeu77@gmail.com>
2021-10-02 16:04:23 -0300

    Modificação View Lazer em Casa Buttons

--- src/views/LazerEmCasa.vue
+++ src/views/LazerEmCasa.vue
@@ -5,8 +5,9 @@
     <p class="texto-informativo">
       Para combater o suicídio, muitas atitudes são necessárias, essas incluem uma boa alimentação, boas noites de sono, exercícios físicos e etc. Mas não é só isso, atividades de lazer não devem ser deixadas de lado. Durante a pandemia, tem sido um grande desafio para muitos encontrar formas criativas de passar tempo em casa. O uso de jogos eletrônicos, programas televisivos, desenhos e animes e outras formas de lazer, estão sendo muito usadas como "passa tempo" nesse período. Sabendo disso, fizemos uma lista de jogos e animes que podem ser um bom "passa tempo".
     </p>
-    <p>Clique em um dos botões abaixo e veja alguns games e animes que separamos para vocês:</p>
-    <v-btn
+    <p class="texto-informativo">Clique em um dos botões abaixo e veja alguns games e animes que separamos para vocês:</p>
+    <div class="mt-15 mb-15">
+      <v-btn
       depressed
       color="primary"
       class="btn-games"
@@ -22,6 +23,7 @@
     >
       Animes
     </v-btn>
+    </div>
   </v-container>
 </template>
 
