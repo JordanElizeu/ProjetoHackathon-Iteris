7482c14013ba5524082063e69d8954523ba3cff6
Natan Menezes <natanmenezes31@gmail.com>
2021-10-02 11:48:10 -0300

    Menu criado

---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-container>
+    <v-navigation-drawer v-model="drawer" app style="background-color:#FECF0A">
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Setembro Amarelo
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Combatendo o suicídio
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app style="background-color:#FECF0A">
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Setembro Amarelo</v-toolbar-title>
+    </v-app-bar>
+
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Menu",
+  data() {
+    return {
+        items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Photos', icon: 'mdi-image' },
+          { title: 'About', icon: 'mdi-help-box' },
+        ],
+        right: null,
+        drawer: null
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
