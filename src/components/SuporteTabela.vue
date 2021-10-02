73871dd317a5773a83cdc5dcc9d7116ad19d0a96
RGonzalesR <renangonzales@gmail.com>
2021-10-02 12:08:50 -0300

    SitesLista rascunho

---
+++ src/components/SuporteTabela.vue
@@ -0,0 +1,51 @@
+<template>
+    <v-simple-table>
+        <template v-slot:default>
+            <thead>
+                <tr>
+                    <th colspan="1" class="text-left">
+                        Posição
+                    </th>
+                    <th colspan="2" class="text-left">
+                        Clube
+                    </th>
+                    <th colspan="3" class="text-left">
+                        Pontos
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr
+                    v-for="(time, index) in times"
+                    :key="time.id"
+                >
+                    <td >{{ index + 1}}</td>
+                    <td >
+                        <v-avatar size="20">
+                            <img :src="time.escudo" :alt="time.nome"/>
+                        </v-avatar>
+                        <span>{{ "   " + time.nome }}</span>
+                    </td>
+                    <td></td>
+                    <td>{{ time.pontos }}</td>
+                </tr>
+            </tbody>
+        </template>
+    </v-simple-table>    
+</template>
+
+<script>
+export default {
+    name: 'ClubesTabela',
+    props: {
+        times: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/SuporteSites.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-container>
+        <h2 class="Titulo">
+            Sites de Apoio
+        </h2>
+        <p>
+            Precisa de ajuda? Clique na imagem
+        </p>
+        <SuporteTabela :sites="SitesOrdenados"/>
+    </v-container>
+</template>
+
+<script>
+import SuporteTabela from '../components/SuporteTabela.vue'
+
+export default {
+    name: 'SitesLista',
+    components: {
+        SuporteTabela
+    },
+    data() {
+        return {
+            SitesLista: []
+        }
+    },
+    created() {
+        fetch('https://it3zxc-default-rtdb.firebaseio.com/setembroamarelo.json')
+        .then(response => response.json())
+        .then(json => {
+            this.SitesLista = json
+        })
+    },
+    computed: {
+        SitesOrdenados (){
+            const listaComputada = this.SitesLista.slice(0).sort(
+                (a, b) => a.pontos > b.pontos ? -1 : 1
+            );
+            return listaComputada;
+        }
+    }
+}
+</script>
