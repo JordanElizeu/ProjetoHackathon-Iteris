02b2cebf1f6888845c1051077c2cfff0552e5bea
Jordan <jordan.elizeu77@gmail.com>
2021-10-02 10:29:32 -0300

    Projeto Padrão

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <HelloWorld/>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components:{
+    HelloWorld    
+  }
+};
+</script>
